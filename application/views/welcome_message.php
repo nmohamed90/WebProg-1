43bffb25bdb863afd01dc6451e485997dab13dfd
escleonh <hescalante@atu.edu>
2019-11-12 16:15:17 -0600

    changed h1

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -68,7 +68,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>Welcome To CodeIgniter!</h1>
+	<h1>Welcome To flkjdaflkjasdkl;fadsfkfjs!</h1>
 
 	<div id="body">
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>

0e683be8afe0d54f0817429a72cc0438719dffed
Zachary Cagle <zcagle2@atu.edu>
2019-11-12 14:33:19 -0800

    First Commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://165.227.250.8/~zcagle2/Lecture/codeigniter/';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -223,7 +223,7 @@ $config['allow_get_array'] = TRUE;
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 1;
+$config['log_threshold'] = 0;
 
 /*
 |--------------------------------------------------------------------------
@@ -324,7 +324,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = 'sDdOa5uC9eTXRgp0OyJqpCUA02olzInqeqhSMtT4r1UzyEoSwUqrPA65e2MMvYN';
+$config['encryption_key'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -377,10 +377,10 @@ $config['encryption_key'] = 'sDdOa5uC9eTXRgp0OyJqpCUA02olzInqeqhSMtT4r1UzyEoSwUq
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'database';
+$config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = 'sessions';
+$config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
